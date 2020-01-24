7b94b5e7c223c40596d9c2dd034faf77704cc6bb
Murilo Miranda <murilomiranda92@hotmail.com>
2020-01-24 06:27:07 -0300

    Updating the drawCircle() function
    I've updated the definition of the drawCircle() function because the previous definition was not working properly.

--- simpledisplay.d
+++ simpledisplay.d
@@ -6604,10 +6604,9 @@ struct ScreenPainter {
 		impl.drawArc(upperLeft.x, upperLeft.y, width, height, start, finish);
 	}
 
-	//this function draws a circle using the drawArc() function above, it requires you to pass the point it
-	//will be drawn at as a Point struct and the radius as an int
+	//this function draws a circle with the drawEllipse() function above, it requires the upper left point and the radius
 	void drawCircle(Point upperLeft, int radius) {
-		this.drawArc(upperLeft, radius, radius, 0, 0);
+		drawEllipse(upperLeft, Point(upperLeft.x + radius, upperLeft.y + radius));
 	}
 
 	/// .
