b95b0e2d6aceda4c273a96a441c3adef23b3c486
Adam D. Ruppe <destructionator@gmail.com>
2020-05-27 10:42:18 -0400

    more screen support

--- terminal.d
+++ terminal.d
@@ -343,7 +343,7 @@ vt|vt100|DEC vt100 compatible:\
 
 
 # Entry for an xterm. Insert mode has been disabled.
-vs|xterm|tmux|tmux-256color|xterm-kitty|screen|screen.xterm|screen.xterm-256color|xterm-color|xterm-256color|vs100|xterm terminal emulator (X Window System):\
+vs|xterm|tmux|tmux-256color|xterm-kitty|screen|screen.xterm|screen-256color|screen.xterm-256color|xterm-color|xterm-256color|vs100|xterm terminal emulator (X Window System):\
 	:am:bs:mi@:km:co#80:li#55:\
 	:im@:ei@:\
 	:cl=\E[H\E[J:\
@@ -751,6 +751,10 @@ struct Terminal {
 			// almost always
 			if(t.indexOf("xterm") != -1)
 				t = "xterm";
+			if(t.indexOf("tmux") != -1)
+				t = "tmux";
+			if(t.indexOf("screen") != -1)
+				t = "screen";
 
 			termcapData = getTermcapDatabase(t);
 		}
