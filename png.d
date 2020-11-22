14a3240323eae7c300a4507d1077902815a642a1
Adam D. Ruppe <destructionator@gmail.com>
2020-11-22 15:48:44 -0500

    a few annotations

--- png.d
+++ png.d
@@ -409,6 +409,7 @@ struct PNG {
 	ubyte[8] header;
 	Chunk[] chunks;
 
+	pure @trusted /* see note on getChunkNullable */
 	Chunk* getChunk(string what) {
 		foreach(ref c; chunks) {
 			if(c.stype == what)
@@ -417,6 +418,7 @@ struct PNG {
 		throw new Exception("no such chunk " ~ what);
 	}
 
+	nothrow @nogc pure @trusted /* trusted because &c i know is referring to the dynamic array, not actually a local. That has lifetime at least as much of the parent PNG object. */
 	Chunk* getChunkNullable(string what) {
 		foreach(ref c; chunks) {
 			if(c.stype == what)
@@ -429,6 +431,7 @@ struct PNG {
 	// so we have to insert our custom chunks right before it.
 	// Use `Chunk.create()` to create new chunk, and then `insertChunk()` to add it.
 	// Return `true` if we did replacement.
+	nothrow pure @trusted /* the chunks.ptr here fails safe, but it does that for performance and again I control that data so can be reasonably assured */
 	bool insertChunk (Chunk* chk, bool replaceExisting=false) {
 		if (chk is null) return false; // just in case
 		// use reversed loop, as "IDAT" is usually present, and it is usually the last,
@@ -452,6 +455,7 @@ struct PNG {
 	}
 
 	// Convenient wrapper for `insertChunk()`.
+	nothrow pure @safe
 	bool replaceChunk (Chunk* chk) { return insertChunk(chk, true); }
 }
 
@@ -863,9 +867,10 @@ ubyte[] getANDMask(PNG* p) {
 
 // Done with assumption
 
+@nogc @safe pure
 PngHeader getHeader(PNG* p) {
 	PngHeader h;
-	ubyte[] data = p.getChunk("IHDR").payload;
+	ubyte[] data = p.getChunkNullable("IHDR").payload;
 
 	int pos = 0;
 
@@ -1003,6 +1008,7 @@ void replacePalette(PNG* p, Color[] colors) {
 		alpha.checksum = crc("tRNS", alpha.payload);
 }
 
+@safe nothrow pure @nogc
 uint update_crc(in uint crc, in ubyte[] buf){
 	static const uint[256] crc_table = [0, 1996959894, 3993919788, 2567524794, 124634137, 1886057615, 3915621685, 2657392035, 249268274, 2044508324, 3772115230, 2547177864, 162941995, 2125561021, 3887607047, 2428444049, 498536548, 1789927666, 4089016648, 2227061214, 450548861, 1843258603, 4107580753, 2211677639, 325883990, 1684777152, 4251122042, 2321926636, 335633487, 1661365465, 4195302755, 2366115317, 997073096, 1281953886, 3579855332, 2724688242, 1006888145, 1258607687, 3524101629, 2768942443, 901097722, 1119000684, 3686517206, 2898065728, 853044451, 1172266101, 3705015759, 2882616665, 651767980, 1373503546, 3369554304, 3218104598, 565507253, 1454621731, 3485111705, 3099436303, 671266974, 1594198024, 3322730930, 2970347812, 795835527, 1483230225, 3244367275, 3060149565, 1994146192, 31158534, 2563907772, 4023717930, 1907459465, 112637215, 2680153253, 3904427059, 2013776290, 251722036, 2517215374, 3775830040, 2137656763, 141376813, 2439277719, 3865271297, 1802195444, 476864866, 2238001368, 4066508878, 1812370925, 453092731, 2181625025, 4111451223, 1706088902, 314042704, 2344532202, 4240017532, 1658658271, 366619977, 2362670323, 4224994405, 1303535960, 984961486, 2747007092, 3569037538, 1256170817, 1037604311, 2765210733, 3554079995, 1131014506, 879679996, 2909243462, 3663771856, 1141124467, 855842277, 2852801631, 3708648649, 1342533948, 654459306, 3188396048, 3373015174, 1466479909, 544179635, 3110523913, 3462522015, 1591671054, 702138776, 2966460450, 3352799412, 1504918807, 783551873, 3082640443, 3233442989, 3988292384, 2596254646, 62317068, 1957810842, 3939845945, 2647816111, 81470997, 1943803523, 3814918930, 2489596804, 225274430, 2053790376, 3826175755, 2466906013, 167816743, 2097651377, 4027552580, 2265490386, 503444072, 1762050814, 4150417245, 2154129355, 426522225, 1852507879, 4275313526, 2312317920, 282753626, 1742555852, 4189708143, 2394877945, 397917763, 1622183637, 3604390888, 2714866558, 953729732, 1340076626, 3518719985, 2797360999, 1068828381, 1219638859, 3624741850, 2936675148, 906185462, 1090812512, 3747672003, 2825379669, 829329135, 1181335161, 3412177804, 3160834842, 628085408, 1382605366, 3423369109, 3138078467, 570562233, 1426400815, 3317316542, 2998733608, 733239954, 1555261956, 3268935591, 3050360625, 752459403, 1541320221, 2607071920, 3965973030, 1969922972, 40735498, 2617837225, 3943577151, 1913087877, 83908371, 2512341634, 3803740692, 2075208622, 213261112, 2463272603, 3855990285, 2094854071, 198958881, 2262029012, 4057260610, 1759359992, 534414190, 2176718541, 4139329115, 1873836001, 414664567, 2282248934, 4279200368, 1711684554, 285281116, 2405801727, 4167216745, 1634467795, 376229701, 2685067896, 3608007406, 1308918612, 956543938, 2808555105, 3495958263, 1231636301, 1047427035, 2932959818, 3654703836, 1088359270, 936918000, 2847714899, 3736837829, 1202900863, 817233897, 3183342108, 3401237130, 1404277552, 615818150, 3134207493, 3453421203, 1423857449, 601450431, 3009837614, 3294710456, 1567103746, 711928724, 3020668471, 3272380065, 1510334235, 755167117];
 
@@ -1649,7 +1655,7 @@ struct BufferedInputRange(Range)
 import std.math;
 
 /// All PNG files are supposed to open with these bytes according to the spec
-enum immutable(ubyte[]) PNG_MAGIC_NUMBER = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
+static immutable(ubyte[]) PNG_MAGIC_NUMBER = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
 
 /// A PNG file consists of the magic number then a stream of chunks. This
 /// struct represents those chunks.
@@ -1660,10 +1666,12 @@ struct Chunk {
 	uint checksum;
 
 	/// returns the type as a string for easier comparison with literals
+	@nogc @safe nothrow pure
 	const(char)[] stype() return const {
 		return cast(const(char)[]) type;
 	}
 
+	@trusted nothrow pure /* trusted because of the cast of name to ubyte. It is copied into a new buffer anyway though so obviously harmless. */
 	static Chunk* create(string type, ubyte[] payload)
 		in {
 			assert(type.length == 4);
@@ -1680,6 +1688,7 @@ struct Chunk {
 	}
 
 	/// Puts it into the format for outputting to a file
+	@safe nothrow pure
 	ubyte[] toArray() {
 		ubyte[] a;
 		a.length = size + 12;
@@ -1750,16 +1759,17 @@ struct PngHeader {
 	/// 0 is non interlaced, 1 if Adam7. No more are defined in the spec
 	ubyte interlaceMethod = 0;
 
+	pure @safe // @nogc with -dip1008 too......
 	static PngHeader fromChunk(in Chunk c) {
-		enforce(c.stype == "IHDR",
-			"The chunk is not an image header");
+		if(c.stype != "IHDR")
+			throw new Exception("The chunk is not an image header");
 
 		PngHeader h;
 		auto data = c.payload;
 		int pos = 0;
 
-		enforce(data.length == 13,
-			"Malformed PNG file - the IHDR is the wrong size");
+		if(data.length != 13)
+			throw new Exception("Malformed PNG file - the IHDR is the wrong size");
 
 		h.width |= data[pos++] << 24;
 		h.width |= data[pos++] << 16;
@@ -1870,6 +1880,7 @@ void writePngLazy(OutputRange, InputRange)(ref OutputRange where, InputRange ima
 
 // bKGD - palette entry for background or the RGB (16 bits each) for that. or 16 bits of grey
 
+@trusted nothrow pure @nogc /* trusted because of the cast from char to ubyte */
 uint crcPng(in char[] chunkName, in ubyte[] buf){
 	uint c = update_crc(0xffffffffL, cast(ubyte[]) chunkName);
 	return update_crc(c, buf) ^ 0xffffffffL;
