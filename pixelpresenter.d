7266c4883ab3ef1787a3e9f9c83e186e532f786e
Elias Batek <desisma@heidel.beer>
2023-12-16 04:31:51 +0100

    Add arsd.pixelpresenter

---
+++ pixelpresenter.d
@@ -0,0 +1,12 @@
+/+
+	== pixelpresenter ==
+	Copyright Elias Batek (0xEAB) 2023.
+	Distributed under the Boost Software License, Version 1.0.
+ +/
+/++
+	# Pixel Presenter
+ +/
+module arsd.pixelpresenter;
+
+import arsd.color;
+import arsd.simpledisplay;
